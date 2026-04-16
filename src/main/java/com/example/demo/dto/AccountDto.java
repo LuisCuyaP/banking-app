2a50c8bccc9f962f65c7708e595c5ed9735ef3ba
DevEnrqiue <luiscuyap@gmail.com>
2026-04-15 19:13:41 -0500

    feat: add finish

--- src/main/java/com/example/demo/dto/AccountDto.java
+++ src/main/java/com/example/demo/dto/AccountDto.java
@@ -1,12 +1,15 @@
 package com.example.demo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//
+//@Data
+//@AllArgsConstructor
+//public class AccountDto {
+//    private Long id;
+//    private String accountHolderName;
+//    private double balance;
+//}
 
-@Data
-@AllArgsConstructor
-public class AccountDto {
-    private Long id;
-    private String accountHolderName;
-    private double balance;
+public record AccountDto(Long id, String accountHolderName, double balance) {
 }
